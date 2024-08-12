420444ddc2024a9c20258b8d9a30d7a44877769e
trunbiz <trunbiz.dev@gmail.com>
2024-08-12 13:38:20 +0700

    webhook ads.txt

--- adsMaxValue.php
+++ adsMaxValue.php
@@ -15,6 +15,8 @@ if ( !defined( 'ABSPATH' ) ) exit;
 // Định nghĩa các hằng số
 define( 'AAP_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'AAP_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define('MY_WEBHOOK_SECRET_KEY', 'SaYoWRY6B9uIgL3QJNBkLw5wiEodXzm7');
+
 require_once plugin_dir_path(__FILE__) . 'includes/controllers/controller.php';
 require_once plugin_dir_path(__FILE__) . 'includes/models/zones.php';
 require_once plugin_dir_path(__FILE__) . 'includes/models/users.php';
@@ -45,13 +47,58 @@ register_uninstall_hook(__FILE__, 'aap_uninstall');
 
 // Tạo các mục menu trong admin
 function aap_admin_menu() {
-    add_menu_page('AdMaxValue Media', 'Ad MaxValue', 'manage_options', 'aap-dashboard', 'AAP_Controller::dashboard', 'dashicons-admin-generic');
+    add_menu_page('Dashboard', 'Ad MaxValue', 'manage_options', 'aap-dashboard', 'AAP_Controller::dashboard', 'dashicons-welcome-widgets-menus');
     add_submenu_page('aap-dashboard', 'Manage Zones', 'Zones', 'manage_options', 'aap-zones', 'AAP_Controller::zones');
     add_submenu_page('aap-dashboard', 'Reports', 'Reports', 'manage_options', 'aap-reports', 'AAP_Controller::reports');
     add_submenu_page('aap-dashboard', 'Wallets', 'Wallets', 'manage_options', 'aap-wallets', 'AAP_Controller::wallets');
     add_submenu_page('aap-dashboard', 'Logins', 'Login', 'manage_options', 'aap-login', 'AAP_Controller::login');
 }
+
+function register_webhook()
+{
+    add_action('rest_api_init', function () {
+        register_rest_route('mv', '/webhook', array(
+            'methods' => 'POST',
+            'callback' => 'handle_my_webhook',
+            'permission_callback' => '__return_true',
+        ));
+    });
+}
+
+function handle_my_webhook(WP_REST_Request $request)
+{
+    $type = $request->get_param('type');
+    $secretKey = $request->get_param('secretKey');
+    if (!empty($secretKey))
+    {
+        if (!empty($type))
+        {
+            switch ($type){
+                case 'ADS.TXT':
+                    $content = $request->get_param('content');
+                    if (empty($content)) {
+                        return new WP_REST_Response('Content is required.', 400);
+                    }
+                    return addAdsTxt($content);
+                default:
+                    break;
+            }
+        }
+    }
+}
+
+function addAdsTxt($content)
+{
+    $file_path = ABSPATH . 'ads.txt';
+    if (file_put_contents($file_path, $content) !== false) {
+        return new WP_REST_Response('ads.txt created successfully.', 200);
+    } else {
+        return new WP_REST_Response('Failed to create ads.txt.', 500);
+    }
+}
+
 add_action('admin_menu', 'aap_admin_menu');
+add_action('init', 'register_webhook');
 
 // Tải các file CSS và JS cần thiết
 function aap_enqueue_assets() {
