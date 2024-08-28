dbb7bd2c40616cff7d8356116edf149c6fa8304a
robert632000 <robert@maxvalue.media>
2024-08-28 16:11:52 +0700

    Add logout fnc

--- templates/logout.php
+++ templates/logout.php
@@ -1,17 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Logging Out...</title>
     <script>
-        localStorage.removeItem('mv_jwt_token');
-        localStorage.removeItem('user_info');
+        var token = localStorage.getItem('mv_jwt_token');
 
-        window.location.href = "<?php echo admin_url('admin.php?page=mv-dashboard'); ?>";
+        fetch('https://stg-publisher.maxvalue.media/api/logout', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`,
+                },
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    localStorage.removeItem('mv_jwt_token');
+                    localStorage.removeItem('user_info');
+
+                    window.location.href = "<?php echo admin_url('admin.php?page=mv-dashboard'); ?>";
+                }
+            });
     </script>
 </head>
+
 <body>
     <p>Logging out...</p>
 </body>
+
 </html>
